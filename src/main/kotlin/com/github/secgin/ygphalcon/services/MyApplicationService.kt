daa2408934ae89395c54454eccc486ab6adbf2d3
GitHub Action <action@github.com>
2022-03-18 21:04:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/secgin/ygphalcon/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/secgin/ygphalcon/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.secgin.ygphalcon.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.secgin.ygphalcon.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/secgin/ygphalcon/services/MyProjectService.kt
+++ src/main/kotlin/com/github/secgin/ygphalcon/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.secgin.ygphalcon.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.secgin.ygphalcon.MyBundle
 
 class MyProjectService(project: Project) {
 
